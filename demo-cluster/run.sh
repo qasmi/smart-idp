62eb46360cb76abfb36c912f30cb11e3db50e3d1
Sara Qasmi <saraqasmi@Saras-MacBook-Pro.local>
2025-07-30 18:49:26 +0200

    init gitops

---
+++ demo-cluster/run.sh
@@ -0,0 +1,18 @@
+path="./.gitops-scripts"
+rm -rf $path && mkdir $path
+echo "applications: []" > $path/config.yaml
+yq eval-all 'select(fileIndex > 0) as $item ireduce ([]; . + $item) | {"applications": .}' \
+$path/config.yaml ./team-*/apps.yaml > $path/tmp.yaml && mv $path/tmp.yaml $path/config.yaml
+yq eval-all '.projects as $item ireduce ({}; . * {"projects": $item})' ./team-*/projects.yaml >> $path/config.yaml
+echo "" >> $path/config.yaml
+yq eval-all '.namespaces as $item ireduce ({}; . * {"namespaces": $item})' ./team-*/namespaces.yaml >> $path/config.yaml
+cat global.yaml >> $path/config.yaml
+
+docker run --rm \
+  -v "${PWD}:/work" \
+  -v "$(realpath ../.gitops-chart):/chart" \
+  -w /work \
+  alpine/helm \
+  template /chart --values $path/config.yaml > ./.k8s-gen-output/manifests.yaml
+  
+rm -rf $path
